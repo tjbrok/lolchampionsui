8fbc0c259dc60a25f603ab83010d2ab914a5074f
tjbrok <106084185+tjbrok@users.noreply.github.com>
2022-05-26 13:44:05 +0200

    Corrected url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true, 
-  apiUrl: "https://lol-champions-ose12.apps.ose.openshift.conygre.com/api/v1/lolchampion/"
+  apiUrl: "https://lol-champions-ose12-champs.apps.ose.openshift.conygre.com/api/v1/lolchampion"
 };
